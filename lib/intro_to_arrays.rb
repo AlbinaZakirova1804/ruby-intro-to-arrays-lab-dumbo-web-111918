971c2ffeec4d24a160915aa3c6e731fc6ecbdad1
Albina Zakirova <zak1804@gmail.com>
2018-11-09 05:45:55 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,6 +6,19 @@ def array_with_two_elements
   @two_elemets_array = ['a', 'b']
 end
 
-def first_elemet
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   
+def first_elemet(taylor_swift)
+    my_first_element = taylor_swift[0]
+end
+
+def third_element(taylor_swift)
+  my_third_element = taylor_swift[2]
+end
+
+def last_element(taylor_swift)
+  my_last_element = taylor_swift[-1]
+end
+
+def first_element_with_array_methods
 end
